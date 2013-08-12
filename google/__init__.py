9490474134ff516f454256ce9c4625aa7f881515
ecsark <ecsark@gmail.com>
2013-08-13 01:47:09 +0800

    add mono word width calculation to confine title length

--- google/__init__.py
+++ google/__init__.py
@@ -24,6 +24,49 @@ class GoogleLens(SingleScopeLens):
     gsearch = GoogleSearch()
 
 
+    def trimTitleNaive(self, titleRaw):
+        titleRaw = titleRaw[:50]
+        title = ""
+        if "-" in titleRaw:
+            title = "-".join(titleRaw.split("-")[:2])
+        else:
+            title = titleRaw
+        if "_" in titleRaw:
+            title = "_".join(titleRaw.split("_")[:3])
+        else:
+            title = titleRaw
+        return title
+
+
+    narrowd = ['i','l','I','.','-',',',' ','!',':']
+
+    def trimTitleMono(self,titleRaw):
+        raw = titleRaw.strip()
+        if "-" in raw:
+            title = "-".join(raw.split("-")[:3])
+        else:
+            title = raw
+        if "_" in raw:
+            title = "_".join(raw.split("_")[:3])
+        else:
+            title = raw
+        title = ""
+        length = 0
+        for uchar in raw:
+            if uchar>= u'\u4e00' and uchar<=u'\u9fa5' or uchar=="W": #Chinese characters
+                length += 3
+            elif uchar in self.narrowd:
+                length += 1
+            elif uchar>=u'\u0041' and  uchar<=u'\u005a': # capital letters
+                length += 2
+            else:
+                length += 1.5
+            if length<65:
+                title+=uchar
+            else:
+                break
+        return title
+
     """
     updated: press space to search
     """
@@ -32,17 +75,11 @@ class GoogleLens(SingleScopeLens):
         if len(search.strip())>=1 and search[-1]==" ":
             answers = self.gsearch.google(search)
             for ans in answers:
-                titleRaw = ans['title'][:45]
-                title = ""
-                if "-" in titleRaw:
-                    title = "-".join(titleRaw.split("-")[:2])
-                else:
-                    title = titleRaw
                 results.append(ans['url'],
                                 'zoom-in',
                                 self.results_category,
                                 "text/html",
-                                title,
+                                self.trimTitleMono(ans['title']),
                                 ans['synop'],
                                 ans['url'])
 
